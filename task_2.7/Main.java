5757d706e603a5b22fdc5648b3e208a6667fe54d
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-27 17:20:58 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    /*
+    Задание 2.7: Выведите в консоль столбец чисел от 1 до 50.
+     */
+    public static void main(String[] args)
+    {
+        for (int i=1; i <= 50; i++)
+        {    System.out.println(i);
+        }
+    }
+}
