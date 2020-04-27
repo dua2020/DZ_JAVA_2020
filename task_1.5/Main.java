7d07c9ec5373ac85cd42126edc29cff54405a9a5
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-27 12:24:15 +0300

    Create Main.java

---
+++ task_1.5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        /* Задание 1.5
+        Напиши программу, которая в методе main объявляет такие переменные:
+        name типа String, age типа int и city типа String.
+        */
+        String name;
+        int age;
+        String city;
+    }
+                }
