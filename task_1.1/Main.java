9d87d843f3250c939295361de315de9cbb5479dd
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-27 11:53:23 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    /*
+    Задание 1.1
+    Напишите фрагмент класса (группу комманд),
+    который будет присваивать значения 5 и 9 двум переменным,
+    а затем третья переменная будет получать значение,
+    равное сумме значений первых двух переменных.
+     */
+    {
+        int iPerem1=5;
+        int iPerem2=9;
+        int iPerem3=0;
+        System.out.println(iPerem3=iPerem1+iPerem2);
+    }
+                   }
