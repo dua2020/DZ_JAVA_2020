d5816beeea8937736e665c635144697a82ac86ae
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-27 12:03:22 +0300

    Create Main.java

---
+++ task_1.2/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        /*
+    Задание 1.2
+    Напишите фрагмент класса, который будет присваивать
+    значение 7.5 переменной х, а затем переменным a и b будет присваивать
+    значения вдвое и второе (соответственно) большие значения переменной х.
+        */
+        int iPerem1=5;
+        int iPerem2=9;
+        int iPerem3=0;
+        double x=7.5;
+        double a=0.0;
+        double b=0.0;
+        System.out.println(a=x*2);
+        System.out.println(b=x*3);
+    }
+                    }
