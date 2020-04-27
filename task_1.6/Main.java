02d6bf16a36163865ed3121148380866d287541c
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-27 12:27:56 +0300

    Create Main.java

---
+++ task_1.6/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        /*
+        Задание 1.6
+        Напиши программу, которая выводит на экран квадрат переменной number;
+         */
+        double iParam=575.0;
+        double iParam_2 = 0.0;
+        double iRezult = 0.0;
+        double iStepen=2.0;
+
+        System.out.println(iParam_2=iParam*iParam);
+        System.out.println(iRezult=Math.pow(iParam,iStepen));
+
+    }
+}
