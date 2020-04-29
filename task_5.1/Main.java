6e537956c734302bcf3f493b56965ec763b6c5a4
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-04-29 23:45:34 +0300

    Create Main.java

---
+++ task_5.1/Main.java
@@ -0,0 +1,21 @@
+public class Cat {
+    private static /* !!! */ String name = "безымянный кот";
+    public void setName(String name) {
+        //напишите тут ваш код
+        this.name = name;
+        // System.out.println(this.name);
+        // System.out.println(name);
+    }
+    /* Задача 5.1: Помогите коту обрести имя с помощью метода setName. */
+     public static void main(String[] args)
+    {
+        // String name = "Мурка";
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        System.out.println(name);
+        cat.setName("Мурка");
+        System.out.println(" или ");
+        System.out.println(name);
+    }
+}
+
