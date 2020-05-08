da6affa2ccf5c6bd57fdba729f6f5b8e78c0a033
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-05-08 09:04:40 +0300

    Create Main.java

---
+++ task_6.6_2/Main.java
@@ -0,0 +1,18 @@
+/* Задача 6.6:
+Перехватить исключение, возникающее при выполнении кода:
+int a = 42 / 0;
+Вывести на экран тип перехваченного исключения.*/
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        try {
+            int a = 42 / 0;
+        } catch (ArithmeticException err) {
+          //  System.out.println ("Делить на ноль нельзя !!!");
+              System.out.println (err);
+          //  err.printStackTrace();
+        }
+        //напишите тут ваш код
+    }
+}
