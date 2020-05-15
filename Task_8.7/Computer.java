c66c2227ee07700c4641170bc58388424c27f502
dua2020 <64191675+dua2020@users.noreply.github.com>
2020-05-16 00:25:02 +0300

    Create Computer.java

---
+++ Task_8.7/Computer.java
@@ -0,0 +1,14 @@
+// класс Computer
+public class Computer
+{
+    private Keyboard keyboard;
+    private Mouse mouse;
+    private Monitor monitor;
+
+    public Computer(Keyboard keyboard, Mouse mouse, Monitor monitor )
+    {
+        this.keyboard = keyboard;
+        this.mouse = mouse;
+        this.monitor = monitor;
+    }
+    
